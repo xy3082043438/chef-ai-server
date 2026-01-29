19be30dc69712c261fddc555c8782844f93f7fad
Qu Xin <xy3082043438@gmail.com>
2026-01-29 14:53:35 +0800

    初始化

--- src/main/java/com/lamb/chefaiserver/ChefAiServerApplication.java
+++ src/main/java/com/lamb/chefaiserver/ChefAiServerApplication.java
@@ -1,6 +1,5 @@
 package com.lamb.chefaiserver;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,7 +14,7 @@ public class ChefAiServerApplication {
      *
      * @param args 启动参数
      */
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(ChefAiServerApplication.class, args);
     }
 
